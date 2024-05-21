e650b2827caac43994b3f98b07739526eaf9346a
My Name <someone@email.com>
2024-05-21 13:25:36 +0500

    portfolio

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ document.getElementById('menu-button').addEventListener('click', function() {
       menu.classList.add('hidden');
     }
   });
+
+  
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    emailjs.sendForm('service_o8zn1tz', 'template_uciluog', this) 
+      .then(function(response) {
+        console.log('Email sent!', response.status, response.text);
+        alert('Email sent successfully!');
+      }, function(error) {
+        console.error('Error sending email:', error);
+        alert('Oops! Something went wrong.');
+      });
+  });
