d1cbc61d6c726de97f7f3c3f17589837d8ef1855
My Name <someone@email.com>
2024-05-21 15:22:24 +0500

    portfolio

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-document.getElementById('menu-button').addEventListener('click', function() {
-    var menu = document.getElementById('mobile-menu');
-    if (menu.classList.contains('hidden')) {
-      menu.classList.remove('hidden');
-    } else {
-      menu.classList.add('hidden');
-    }
-  });
-
-  function senndMail (){
-    let parms = {
-      name : document.getElementById("name").value,
-      email : document.getElementById("email").value,
-      subject :document.getElementById("message").value,
-    }
-  
-    emailjs.send("service_qua4djz","template_ly95juk",parms).then(alert("Email Sent!!"))
+document.getElementById("menu-button").addEventListener("click", function () {
+  var menu = document.getElementById("mobile-menu");
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden");
   }
+});
+
+function sendMail() {
+  event.preventDefault(); // Prevent the form from submitting the default way
+  emailjs
+    .sendForm(
+      "service_qua4djz",
+      "template_ly95juk",
+      document.getElementById("contact-form")
+    )
+    .then(
+      function (response) {
+        console.log("SUCCESS!", response.status, response.text);
+        alert("Email sent successfully!");
+      },
+      function (error) {
+        console.error("FAILED...", error);
+        alert("Oops! Something went wrong.");
+      }
+    );
+}
