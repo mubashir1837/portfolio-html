8fb6552222f96bca82b131ae834b22bea9c192c5
My Name <someone@email.com>
2024-05-29 11:35:50 +0500

    portfolio

--- script.js
+++ script.js
@@ -7,22 +7,35 @@ document.getElementById("menu-button").addEventListener("click", function () {
   }
 });
 
-function sendMail() {
-  event.preventDefault();
-  emailjs
-    .sendForm(
-      "service_qua4djz",
-      "template_ly95juk",
-      document.getElementById("contact-form")
-    )
+function sendMail(event) {
+  event.preventDefault(); 
+  var form = document.getElementById('form');
+  
+  var templateParams = {
+    from_name: form.name.value,
+    from_email: form.email.value,
+    message: form.message.value,
+    reply_to: form.email.value
+  };
+
+  emailjs.send("service_qua4djz", "template_ly95juk", templateParams)
     .then(
-      function (response) {
+      function(response) {
         console.log("SUCCESS!", response.status, response.text);
         alert("Email sent successfully!");
       },
-      function (error) {
+      function(error) {
         console.error("FAILED...", error);
         alert("Oops! Something went wrong.");
       }
     );
 }
+
+
+
+
+
+document.getElementById('menu-button').addEventListener('click', function () {
+  var menu = document.getElementById('mobile-menu');
+  menu.classList.toggle('hidden');
+});zz
